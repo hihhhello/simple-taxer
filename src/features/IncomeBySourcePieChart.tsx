ab3ee53b8488199dbb0ad5bb70bc6f90b93d3368
hihhhello <grigori.ratushniak@gmail.com>
2023-10-05 20:48:12 -0500

    add pie chart docs example

--- src/features/IncomeBySourcePieChart.tsx
+++ src/features/IncomeBySourcePieChart.tsx
@@ -76,6 +76,41 @@ export const IncomeBySourcePieChart = ({
       .attr('height', height)
       .attr('viewBox', [-width / 2, -height / 2, width, height])
       .attr('style', 'max-width: 100%; height: auto; font: 10px sans-serif;');
+
+    svg
+      .append('g')
+      .attr('stroke', 'white')
+      .selectAll()
+      .data(arcs)
+      .join('path')
+      .attr('fill', (d) => color(d.data.name))
+      .attr('d', arc)
+      .append('title')
+      .text((d) => `${d.data.name}: ${d.data.value.toLocaleString('en-US')}`);
+
+    svg
+      .append('g')
+      .attr('text-anchor', 'middle')
+      .selectAll()
+      .data(arcs)
+      .join('text')
+      .attr('transform', (d) => `translate(${arcLabel.centroid(d)})`)
+      .call((text) =>
+        text
+          .append('tspan')
+          .attr('y', '-0.4em')
+          .attr('font-weight', 'bold')
+          .text((d) => d.data.name),
+      )
+      .call((text) =>
+        text
+          .filter((d) => d.endAngle - d.startAngle > 0.25)
+          .append('tspan')
+          .attr('x', 0)
+          .attr('y', '0.7em')
+          .attr('fill-opacity', 0.7)
+          .text((d) => d.data.value.toLocaleString('en-US')),
+      );
   }, []);
 
   return <svg ref={ref} />;
