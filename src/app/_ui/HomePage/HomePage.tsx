7178f2102219a3cca31a98aa7d2454b68f7de2d1
hihhhello <grigori.ratushniak@gmail.com>
2023-10-07 23:13:15 -0500

    adjust mobile view for transactions search input

--- src/app/_ui/HomePage/HomePage.tsx
+++ src/app/_ui/HomePage/HomePage.tsx
@@ -241,7 +241,7 @@ export const HomePageContent = ({
                   <input
                     name="transactionSearchQuery"
                     id="transactionSearchQuery"
-                    className="block rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:w-auto sm:text-sm sm:leading-6"
                     placeholder="Search source or bank name"
                     value={transactionSearchQuery}
                     onChange={handleChangeTransactionSearchQuery}
