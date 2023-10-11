47b99204cfab5e919bd6297681f65280175162b7
hihhhello <grigori.ratushniak@gmail.com>
2023-10-11 15:43:37 -0500

    add nextjs OG images

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,19 @@ export const metadata: Metadata = {
     siteName: 'Simple Taxer',
     locale: 'en_US',
     type: 'website',
+    images: [
+      {
+        url: 'https://nextjs.org/og.png',
+        width: 800,
+        height: 600,
+      },
+      {
+        url: 'https://nextjs.org/og-alt.png',
+        width: 1800,
+        height: 1600,
+        alt: 'My custom alt',
+      },
+    ],
   },
 };
 
