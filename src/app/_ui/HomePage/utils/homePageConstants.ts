b5799986db302cf9d7d142f6bba60285dfb3578f
hihhhello <grigori.ratushniak@gmail.com>
2023-10-17 17:55:34 -0500

    add open graph per tab description

---
+++ src/app/_ui/HomePage/utils/homePageConstants.ts
@@ -0,0 +1,10 @@
+import { HomePageTab, HomePageTabKey } from './homePageTypes';
+
+export const HOME_PAGE_TAB_KEY_TO_OPEN_GRAPH_DESCRIPTION: Record<
+  HomePageTab,
+  string
+> = {
+  [HomePageTabKey.ANALYTICS]: 'Check how much you got from each source.',
+  [HomePageTabKey.CALCULATOR]: 'Get your taxes calculated in seconds.',
+  [HomePageTabKey.TRANSACTIONS]: 'Keep your income recorded.',
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import { getServerSession } from 'next-auth';
 import { Metadata } from 'next';
+import { upperFirst } from 'lodash';
 
 import { createAuthorizedCaller } from '@/server';
 import { HomePageContent } from '@/app/_ui/HomePage/HomePage';
 import { NEXT_AUTH_OPTIONS } from '@/app/api/auth/[...nextauth]/route';
-import { HomePageTab } from './_ui/HomePage/utils/homePageTypes';
-import { upperFirst } from 'lodash';
+import {
+  HomePageTab,
+  HomePageTabKey,
+} from './_ui/HomePage/utils/homePageTypes';
+import { HOME_PAGE_TAB_KEY_TO_OPEN_GRAPH_DESCRIPTION } from './_ui/HomePage/utils/homePageConstants';
 
 type HomeProps = {
   searchParams: {
@@ -38,5 +42,11 @@ export async function generateMetadata({
 }: HomeProps): Promise<Metadata> {
   return {
     title: `Simple Taxer | ${upperFirst(searchParams.tab) ?? 'Transactions'}`,
+    openGraph: {
+      description:
+        HOME_PAGE_TAB_KEY_TO_OPEN_GRAPH_DESCRIPTION[
+          searchParams.tab ?? HomePageTabKey.TRANSACTIONS
+        ],
+    },
   };
 }
