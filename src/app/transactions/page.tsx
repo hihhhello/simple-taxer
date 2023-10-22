6ddb5c21464629f88b576028c0176482eac3fc8d
hihhhello <grigori.ratushniak@gmail.com>
2023-10-22 09:31:36 -0500

    add antransactions page route

---
+++ src/app/transactions/page.tsx
@@ -0,0 +1,11 @@
+import type { Metadata } from 'next';
+
+const TransactionsPage = () => {
+  return <>Transactions</>;
+};
+
+export const metadata: Metadata = {
+  title: 'Transactions',
+};
+
+export default TransactionsPage;
