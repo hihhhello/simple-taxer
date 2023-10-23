000b2d8e982e6bf1b0641100685c00836b93d111
hihhhello <grigori.ratushniak@gmail.com>
2023-10-23 17:58:26 -0500

    add primary blue colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,12 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        primary: {
+          blue: '#13165C',
+          'light-blue': '#306FD8',
+        },
+      },
     },
   },
   plugins: [require('@tailwindcss/forms')],
