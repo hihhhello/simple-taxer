a77ad18cc2be16f3c05e1aab868650cb7aee5812
hihhhello <grigori.ratushniak@gmail.com>
2023-10-23 18:02:47 -0500

    add navbar links active state

--- src/app/_ui/Navbar.tsx
+++ src/app/_ui/Navbar.tsx
@@ -4,6 +4,8 @@ import { useCallback } from 'react';
 import { signOut } from 'next-auth/react';
 import { Session } from 'next-auth';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { twMerge } from 'tailwind-merge';
 
 type TopbarProps = {
   session: Session | null;
@@ -11,6 +13,8 @@ type TopbarProps = {
 
 export const Topbar = ({ session }: TopbarProps) => {
   const handleSignOut = useCallback(() => signOut(), []);
+
+  const pathname = usePathname();
 
   return (
     <div className="flex items-center justify-between rounded-[100px] bg-white py-4 pl-10 pr-6">
@@ -21,15 +25,36 @@ export const Topbar = ({ session }: TopbarProps) => {
 
         <div>
           <Link href="/transactions">
-            <span className="text-primary-blue px-4 py-2">Transactions</span>
+            <span
+              className={twMerge(
+                'text-primary-blue rounded-3xl px-4 py-2',
+                pathname === '/transactions' && 'bg-primary-blue text-white',
+              )}
+            >
+              Transactions
+            </span>
           </Link>
 
           <Link href="/analytics">
-            <span className="text-primary-blue px-4 py-2">Analytics</span>
+            <span
+              className={twMerge(
+                'text-primary-blue rounded-3xl px-4 py-2',
+                pathname === '/analytics' && 'bg-primary-blue text-white',
+              )}
+            >
+              Analytics
+            </span>
           </Link>
 
           <Link href="/calculator">
-            <span className="text-primary-blue px-4 py-2">Calculator</span>
+            <span
+              className={twMerge(
+                'text-primary-blue rounded-3xl px-4 py-2',
+                pathname === '/calculator' && 'bg-primary-blue text-white',
+              )}
+            >
+              Calculator
+            </span>
           </Link>
         </div>
       </div>
