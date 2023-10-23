08274cab0bc600219ceb45f8e644573223d4b603
hihhhello <grigori.ratushniak@gmail.com>
2023-10-23 18:06:59 -0500

    add redirect from home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,52 +1,14 @@
 import { getServerSession } from 'next-auth';
-import { Metadata } from 'next';
-import { upperFirst } from 'lodash';
 
-import { createAuthorizedCaller } from '@/server';
-import { HomePageContent } from '@/app/_ui/HomePage/HomePage';
 import { NEXT_AUTH_OPTIONS } from '@/app/api/auth/[...nextauth]/route';
-import {
-  HomePageTab,
-  HomePageTabKey,
-} from './_ui/HomePage/utils/homePageTypes';
-import { HOME_PAGE_TAB_KEY_TO_OPEN_GRAPH_DESCRIPTION } from './_ui/HomePage/utils/homePageConstants';
+import { redirect } from 'next/navigation';
 
-type HomeProps = {
-  searchParams: {
-    tab?: HomePageTab;
-  };
-};
-
-export default async function Home({ searchParams }: HomeProps) {
-  const caller = await createAuthorizedCaller();
-
+export default async function Home() {
   const session = await getServerSession(NEXT_AUTH_OPTIONS);
 
-  const transactions = await caller.transactions.getAll({});
-  const sourceIncomes = await caller.transactions.getBySourceName({});
+  if (!session?.user) {
+    return redirect('/calculator');
+  }
 
-  return (
-    <HomePageContent
-      transactions={transactions}
-      me={session?.user}
-      sourceIncomes={sourceIncomes}
-      tab={searchParams.tab}
-    />
-  );
-}
-
-export async function generateMetadata({
-  searchParams,
-}: HomeProps): Promise<Metadata> {
-  return {
-    title: `Simple Taxer | ${
-      searchParams.tab ? upperFirst(searchParams.tab) : 'Transactions'
-    }`,
-    openGraph: {
-      description:
-        HOME_PAGE_TAB_KEY_TO_OPEN_GRAPH_DESCRIPTION[
-          searchParams.tab ?? HomePageTabKey.TRANSACTIONS
-        ],
-    },
-  };
+  return redirect('/transactions');
 }
