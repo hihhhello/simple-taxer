4683cf0ae649ccf8cf5e125e787e019b3b01830c
hihhhello <grigori.ratushniak@gmail.com>
2023-10-24 19:46:20 -0500

    split income tax calculator on form and results

--- src/app/calculator/_ui/CalculatorPageContent.tsx
+++ src/app/calculator/_ui/CalculatorPageContent.tsx
@@ -1,11 +1,25 @@
 'use client';
 
-import { useMemo } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 import { User } from 'next-auth';
 
-import { IncomeTaxCalculator } from '@/features/IncomeTaxCalculator/IncomeTaxCalculator';
+import { IncomeTaxCalculatorForm } from '@/features/IncomeTaxCalculator/IncomeTaxCalculatorForm';
 import { api } from '@/shared/api';
-import { calculateTotalIncome } from '@/shared/utils/helpers';
+import {
+  calculateTotalIncome,
+  formatToUSDCurrency,
+} from '@/shared/utils/helpers';
+import {
+  IncomeTaxCalculatorFilingStatus,
+  IncomeTaxCalculatorFilingStatusKey,
+  IncomeTaxCalculatorUSState,
+} from '@/features/IncomeTaxCalculator/utils/incomeTaxCalculatorTypes';
+import usIncomeTaxes2023 from '@/shared/data/usIncomeTaxes2023.json';
+import {
+  calculateFederalTax,
+  calculateStateTax,
+} from '@/features/IncomeTaxCalculator/utils/incomeTaxCalculatorHelpers';
+import { IncomeTaxCalculatorResults } from '@/features/IncomeTaxCalculator/ui/IncomeTaxCalculatorResults';
 
 type CalculatorPageContentProps = {
   transactions: ApiRouterOutputs['transactions']['getAll'];
@@ -16,6 +30,14 @@ export const CalculatorPageContent = ({
   transactions: initialTransactions,
   me,
 }: CalculatorPageContentProps) => {
+  const [filingStatus, setFilingStatus] =
+    useState<IncomeTaxCalculatorFilingStatus>(
+      IncomeTaxCalculatorFilingStatusKey.SINGLE,
+    );
+  const [householdIncome, setHouseholdIncome] = useState<number | null>(null);
+  const [taxStateKey, setTaxStateKey] =
+    useState<IncomeTaxCalculatorUSState | null>(null);
+
   const { data: transactions } = api.transactions.getAll.useQuery(
     {},
     {
@@ -23,10 +45,116 @@ export const CalculatorPageContent = ({
     },
   );
 
+  const handleCalculateTaxes = useCallback(
+    (values: {
+      householdIncome: number;
+      filingStatus: IncomeTaxCalculatorFilingStatus;
+      taxStateKey: IncomeTaxCalculatorUSState;
+    }) => {
+      setFilingStatus(values.filingStatus);
+      setHouseholdIncome(values.householdIncome);
+      setTaxStateKey(values.taxStateKey);
+    },
+    [],
+  );
+
+  const federalFilingBrackets =
+    filingStatus === IncomeTaxCalculatorFilingStatusKey.SINGLE
+      ? usIncomeTaxes2023.federalSingleFilersIncomeTaxBrackets
+      : usIncomeTaxes2023.federalMarriedFilersIncomeTaxBrackets;
+
+  const federalTax = householdIncome
+    ? calculateFederalTax(householdIncome, federalFilingBrackets)
+    : undefined;
+
+  const stateTax = useMemo(() => {
+    if (!householdIncome || !taxStateKey) {
+      return;
+    }
+
+    const taxState = usIncomeTaxes2023.stateIncomeTaxesByStates.find(
+      ({ key }) => key === taxStateKey,
+    );
+
+    if (!taxState) {
+      return;
+    }
+
+    return calculateStateTax({
+      income: householdIncome,
+      taxStateBrackets: taxState.brackets[filingStatus],
+    });
+  }, [filingStatus, householdIncome, taxStateKey]);
+
   const totalIncome = useMemo(
     () => calculateTotalIncome(transactions?.data),
     [transactions],
   );
 
-  return <IncomeTaxCalculator totalIncome={totalIncome} me={me} />;
+  return (
+    <>
+      <IncomeTaxCalculatorForm
+        handleCalculateTaxes={handleCalculateTaxes}
+        totalIncome={totalIncome}
+        me={me}
+      />
+
+      {federalTax && householdIncome && (
+        <>
+          <IncomeTaxCalculatorResults
+            federalTax={federalTax}
+            householdIncome={householdIncome}
+            stateTax={stateTax}
+          />
+
+          <div className="flex flex-wrap gap-4">
+            {federalFilingBrackets.map(
+              ({ rate, upper: bracketUpper, lower }, index) => {
+                const upper = bracketUpper ?? Number.POSITIVE_INFINITY;
+
+                return (
+                  <div key={rate}>
+                    <span>{rate * 100}%</span>
+
+                    <div
+                      style={{
+                        width: BASE_BRACKET_WIDTH * (index + 1),
+                      }}
+                      className="relative h-[90px] ring-1"
+                    >
+                      <div
+                        style={{
+                          width:
+                            householdIncome > upper ||
+                            (householdIncome <= upper &&
+                              householdIncome > lower)
+                              ? `${Math.min(
+                                  100,
+                                  (householdIncome / upper) * 100,
+                                )}%`
+                              : 0,
+                        }}
+                        className={`absolute left-0 top-0 h-full bg-red-500`}
+                      >
+                        {householdIncome <= upper &&
+                          householdIncome > lower && (
+                            <span className="absolute -right-1/2 top-1/2 -translate-y-1/2">
+                              {formatToUSDCurrency(householdIncome)}
+                            </span>
+                          )}
+                      </div>
+                    </div>
+
+                    <p className="text-right">{formatToUSDCurrency(upper)}</p>
+                  </div>
+                );
+              },
+            )}
+          </div>
+        </>
+      )}
+    </>
+  );
 };
+
+const BASE_BRACKET_WIDTH = 50;
--- src/features/IncomeTaxCalculator/IncomeTaxCalculatorForm.tsx
+++ src/features/IncomeTaxCalculator/IncomeTaxCalculatorForm.tsx
@@ -1,13 +1,7 @@
-import { ChangeEvent, useCallback, useMemo, useState } from 'react';
+import { ChangeEvent, useCallback, useState } from 'react';
 import { User } from 'next-auth';
 
 import { DollarInput } from '@/shared/ui/DollarInput';
-import { formatToUSDCurrency } from '@/shared/utils/helpers';
-import usIncomeTaxes2023 from '@/shared/data/usIncomeTaxes2023.json';
-import {
-  calculateFederalTax,
-  calculateStateTax,
-} from './utils/incomeTaxCalculatorHelpers';
 import {
   IncomeTaxCalculatorFilingStatus,
   IncomeTaxCalculatorFilingStatusKey,
@@ -17,17 +11,22 @@ import {
   INCOME_TAX_CALCULATOR_US_STATES_KEYS,
   INCOME_TAX_CALCULATOR_US_STATE_KEY_TO_NAME,
 } from './utils/incomeTaxCalculatorConstants';
-import { IncomeTaxCalculatorResults } from './ui/IncomeTaxCalculatorResults';
 
-type IncomeTaxCalculatorProps = {
+type IncomeTaxCalculatorFormProps = {
   totalIncome: number | undefined;
   me: User | undefined | null;
+  handleCalculateTaxes: (params: {
+    householdIncome: number;
+    filingStatus: IncomeTaxCalculatorFilingStatus;
+    taxStateKey: IncomeTaxCalculatorUSState;
+  }) => void;
 };
 
-export const IncomeTaxCalculator = ({
+export const IncomeTaxCalculatorForm = ({
   totalIncome,
   me,
-}: IncomeTaxCalculatorProps) => {
+  handleCalculateTaxes,
+}: IncomeTaxCalculatorFormProps) => {
   const [filingStatus, setFilingStatus] =
     useState<IncomeTaxCalculatorFilingStatus>(
       IncomeTaxCalculatorFilingStatusKey.SINGLE,
@@ -35,34 +34,6 @@ export const IncomeTaxCalculator = ({
   const [householdIncome, setHouseholdIncome] = useState<number | null>(null);
   const [taxStateKey, setTaxStateKey] =
     useState<IncomeTaxCalculatorUSState | null>(null);
-
-  const federalFilingBrackets =
-    filingStatus === IncomeTaxCalculatorFilingStatusKey.SINGLE
-      ? usIncomeTaxes2023.federalSingleFilersIncomeTaxBrackets
-      : usIncomeTaxes2023.federalMarriedFilersIncomeTaxBrackets;
-
-  const federalTax = householdIncome
-    ? calculateFederalTax(householdIncome, federalFilingBrackets)
-    : undefined;
-
-  const stateTax = useMemo(() => {
-    if (!householdIncome || !taxStateKey) {
-      return;
-    }
-
-    const taxState = usIncomeTaxes2023.stateIncomeTaxesByStates.find(
-      ({ key }) => key === taxStateKey,
-    );
-
-    if (!taxState) {
-      return;
-    }
-
-    return calculateStateTax({
-      income: householdIncome,
-      taxStateBrackets: taxState.brackets[filingStatus],
-    });
-  }, [filingStatus, householdIncome, taxStateKey]);
 
   const handleUseTotalIncome = useCallback(() => {
     if (!totalIncome) {
@@ -85,6 +56,14 @@ export const IncomeTaxCalculator = ({
     },
     [],
   );
+
+  const handleCalculate = () => {
+    if (!householdIncome || !taxStateKey) {
+      return;
+    }
+
+    handleCalculateTaxes({ filingStatus, householdIncome, taxStateKey });
+  };
 
   return (
     <div>
@@ -170,62 +149,7 @@ export const IncomeTaxCalculator = ({
         </div>
       </div>
 
-      {federalTax && householdIncome && (
-        <>
-          <IncomeTaxCalculatorResults
-            federalTax={federalTax}
-            householdIncome={householdIncome}
-            stateTax={stateTax}
-          />
-
-          <div className="flex flex-wrap gap-4">
-            {federalFilingBrackets.map(
-              ({ rate, upper: bracketUpper, lower }, index) => {
-                const upper = bracketUpper ?? Number.POSITIVE_INFINITY;
-
-                return (
-                  <div key={rate}>
-                    <span>{rate * 100}%</span>
-
-                    <div
-                      style={{
-                        width: BASE_BRACKET_WIDTH * (index + 1),
-                      }}
-                      className="relative h-[90px] ring-1"
-                    >
-                      <div
-                        style={{
-                          width:
-                            householdIncome > upper ||
-                            (householdIncome <= upper &&
-                              householdIncome > lower)
-                              ? `${Math.min(
-                                  100,
-                                  (householdIncome / upper) * 100,
-                                )}%`
-                              : 0,
-                        }}
-                        className={`absolute left-0 top-0 h-full bg-red-500`}
-                      >
-                        {householdIncome <= upper &&
-                          householdIncome > lower && (
-                            <span className="absolute -right-1/2 top-1/2 -translate-y-1/2">
-                              {formatToUSDCurrency(householdIncome)}
-                            </span>
-                          )}
-                      </div>
-                    </div>
-
-                    <p className="text-right">{formatToUSDCurrency(upper)}</p>
-                  </div>
-                );
-              },
-            )}
-          </div>
-        </>
-      )}
+      <button onClick={handleCalculate}>Calculate</button>
     </div>
   );
 };
-
-const BASE_BRACKET_WIDTH = 50;
