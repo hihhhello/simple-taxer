e1e8193896c51131f204a4a14a63fe9a03afc784
hihhhello <grigori.ratushniak@gmail.com>
2023-10-29 18:51:53 -0500

    hide total income widget on mobile

--- src/app/_ui/BaseLayout.tsx
+++ src/app/_ui/BaseLayout.tsx
@@ -21,7 +21,7 @@ export const BaseLayout = async ({ children }: BaseLayoutProps) => {
 
   return (
     <div className="relative min-h-full px-4 pt-4 sm:px-[220px] sm:pt-1">
-      <div className="fixed right-0 top-[116px] flex flex-col rounded-l-2xl bg-primary-light-blue py-4 pl-6 pr-9 font-semibold text-white">
+      <div className="fixed right-0 top-[116px] hidden flex-col rounded-l-2xl bg-primary-light-blue py-4 pl-6 pr-9 font-semibold text-white sm:flex">
         <span>Total Income</span>
 
         <span className="text-3xl">{formatUSDInteger(totalIncome)}</span>
