3d7716d5c850e6e038981b5e32c96c6d9205856c
hihhhello <grigori.ratushniak@gmail.com>
2023-10-31 17:34:23 -0500

    add mobile total income widget

--- src/app/_ui/TotalIncomeWidget.tsx
+++ src/app/_ui/TotalIncomeWidget.tsx
@@ -1,17 +1,32 @@
 'use client';
 
 import { formatUSDInteger } from '@/shared/utils/helpers';
+import { useIsDesktop } from '@/shared/utils/hooks';
 
 type TotalIncomeWidgetProps = {
   totalIncome: number;
 };
 
 export const TotalIncomeWidget = ({ totalIncome }: TotalIncomeWidgetProps) => {
-  return (
-    <div className="fixed right-0 top-[116px] hidden flex-col rounded-l-2xl bg-primary-light-blue py-4 pl-6 pr-9 font-semibold text-white sm:flex">
-      <span>Total Income</span>
+  const isDesktop = useIsDesktop();
 
-      <span className="text-3xl">{formatUSDInteger(totalIncome)}</span>
+  if (isDesktop) {
+    return (
+      <div className="fixed right-0 top-[116px] flex flex-col rounded-l-2xl bg-primary-light-blue py-4 pl-6 pr-9 font-semibold text-white">
+        <span>Total Income</span>
+
+        <span className="text-3xl">{formatUSDInteger(totalIncome)}</span>
+      </div>
+    );
+  }
+
+  return (
+    <div className="fixed bottom-2 left-0 flex items-center gap-1 rounded-r-lg bg-primary-light-blue p-2 pl-4 font-semibold text-white">
+      <div className="flex items-center gap-1">
+        <span className="text-sm">Total Income:</span>
+
+        <span className="text-xl">{formatUSDInteger(totalIncome)}</span>
+      </div>
     </div>
   );
 };
