a43dd94bbbdfb119bb1f8ff2f47e49cd20ec1c0e
hihhhello <grigori.ratushniak@gmail.com>
2023-11-01 19:11:45 -0500

    adjust tax exp card styles. add details button

--- src/app/calculator/_ui/CalculatorTaxExplanationCard/CalculatorTaxExplanationCard.tsx
+++ src/app/calculator/_ui/CalculatorTaxExplanationCard/CalculatorTaxExplanationCard.tsx
@@ -1,3 +1,4 @@
+import { ChevronLeftIcon } from '@/shared/icons/ChevronLeftIcon';
 import { classNames, formatUSDDecimal } from '@/shared/utils/helpers';
 
 type CalculatorTaxExplanationCardProps = {
@@ -22,21 +23,23 @@ export const CalculatorTaxExplanationCard = ({
           'absolute -bottom-3 -left-1 -z-10 h-[115%] w-full rounded-lg sm:-left-2 sm:-top-2 sm:h-full sm:rounded-3xl',
           shadowBgClassName,
         )}
-      ></div>
+      />
 
       <div className="flex items-start justify-between sm:mb-4">
-        <div className="flex items-center justify-center rounded-full bg-primary-yellow px-4 py-2">
-          <span className="text-primary-blue">{title}</span>
+        <div className="flex items-center justify-center rounded-full bg-primary-yellow px-4 py-1 text-sm sm:py-2 sm:text-base">
+          <span className="leading-tight text-primary-blue">{title}</span>
         </div>
 
         <div className="flex gap-2">
-          <div className="flex items-center justify-center rounded-full bg-primary-blue px-4 py-2">
-            <span className="text-white">{formatUSDDecimal(amount)}</span>
+          <div className="rounded-full bg-primary-blue px-4 py-1 text-sm sm:py-2 sm:text-base">
+            <span className="leading-tight text-white">
+              {formatUSDDecimal(amount)}
+            </span>
           </div>
 
           {Boolean(percent) && (
-            <div className="flex h-9 w-9 items-center justify-center rounded-full bg-primary-blue">
-              <span className="text-2xs leading-none text-white">
+            <div className="flex h-[26px] w-[26px] items-center justify-center rounded-full bg-primary-blue sm:h-9 sm:w-9">
+              <span className="text-[8px] leading-tight text-white sm:text-2xs">
                 {percent?.toFixed(2)}%
               </span>
             </div>
@@ -48,8 +51,14 @@ export const CalculatorTaxExplanationCard = ({
         <span className="text-sm font-semibold text-primary-light-blue">
           Exp.:
         </span>
+
         {description}
       </p>
+
+      <button className="absolute -bottom-3 left-1/2 flex -translate-x-1/2 items-center justify-center text-[8px] text-white sm:hidden">
+        <span>Details</span>
+        <ChevronLeftIcon className="h-[12px] -rotate-90" />
+      </button>
     </div>
   );
 };
