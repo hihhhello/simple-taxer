7ae5942d112a3877797b2995e7c905d483476bc3
hihhhello <grigori.ratushniak@gmail.com>
2023-11-04 12:57:43 -0500

    ad htmlFor for InputWrapper

--- src/shared/ui/InputWrapper.tsx
+++ src/shared/ui/InputWrapper.tsx
@@ -1,15 +1,20 @@
-import { ReactNode } from 'react';
+import { LabelHTMLAttributes, ReactNode } from 'react';
 
 type InputWrapperProps = {
   label: string;
   children: ReactNode;
+  htmlFor: LabelHTMLAttributes<HTMLLabelElement>['htmlFor'];
 };
 
-export const InputWrapper = ({ children, label }: InputWrapperProps) => (
+export const InputWrapper = ({
+  children,
+  label,
+  htmlFor,
+}: InputWrapperProps) => (
   <div>
     <div className="mb-1">
       <label
-        htmlFor="date"
+        htmlFor={htmlFor}
         className="rounded-md bg-primary-green px-4 py-1 text-xs text-white sm:text-sm"
       >
         {label}
