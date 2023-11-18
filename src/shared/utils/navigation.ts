bd84912927efca0d1d76233b8790fb75b68d5d8f
hihhhello <grigori.ratushniak@gmail.com>
2023-11-17 22:22:51 -0600

    add navigation items array

---
+++ src/shared/utils/navigation.ts
@@ -0,0 +1,17 @@
+export const NAVIGATION_ITEMS = [
+  {
+    key: 'transactions',
+    href: '/transactions',
+    title: 'Transactions',
+  },
+  {
+    key: 'analytics',
+    href: '/analytics',
+    title: 'Analytics',
+  },
+  {
+    key: 'calculator',
+    href: '/calculator',
+    title: 'Calculator',
+  },
+] as const;
