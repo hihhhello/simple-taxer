037a361da8ebb4f33a744ebb05183e06e85e0c2e
hihhhello <grigori.ratushniak@gmail.com>
2023-11-29 17:58:54 -0600

    add useBoolean hook

--- src/shared/utils/hooks.ts
+++ src/shared/utils/hooks.ts
@@ -69,3 +69,22 @@ export const useIsBreakpoint = (breakpoint: Breakpoints | number) => {
 
   return width > breakpoint;
 };
+
+export const useBoolean = (initialValue: boolean) => {
+  const [value, setValue] = useState(initialValue);
+
+  const setFalse = useCallback(() => setValue(false), []);
+  const setTrue = useCallback(() => setValue(true), []);
+  const toggle = useCallback(() => setValue((prevValue) => !prevValue), []);
+
+  return useMemo(
+    () => ({
+      value,
+      setFalse,
+      setTrue,
+      toggle,
+      setValue,
+    }),
+    [setFalse, setTrue, toggle, value],
+  );
+};
