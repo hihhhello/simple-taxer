978a9745092f53bc4e0f200d6fddb4cc798d1b4a
hihhhello <grigori.ratushniak@gmail.com>
2023-12-08 17:10:23 -0600

    fix svg casing

--- src/shared/icons/BurgerMenuIcon.tsx
+++ src/shared/icons/BurgerMenuIcon.tsx
@@ -13,21 +13,21 @@ export const BurgerMenuIcon = ({ ...svgIconProps }: BurgerMenuIconProps) => {
     >
       <path
         d="M3 12H21"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M3 6H21"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M3 18H21"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
