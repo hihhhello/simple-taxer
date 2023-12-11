e90610959d943c30d18d40a365ddada8c95c1c74
hihhhello <grigori.ratushniak@gmail.com>
2023-12-11 17:26:46 -0600

    add analytics page title card

--- src/app/analytics/_ui/AnalyticsPageContent.tsx
+++ src/app/analytics/_ui/AnalyticsPageContent.tsx
@@ -7,6 +7,8 @@ import { IncomeBySourcePieChart } from '@/features/IncomeBySourcePieChart';
 import { AnalyticsSourceIncome } from '@/shared/types/analyticsTypes';
 import { api } from '@/shared/api';
 import { AnalyticsPageLogInCard } from './AnalyticsPageLogInCard';
+import { Breakpoints, useIsBreakpoint } from '@/shared/utils/hooks';
+import { PieChartHeroIllustrationDesktop } from '@/shared/illustartions/PieChartHeroIllustrationDesktop';
 
 type AnalyticsPageContentProps = {
   me: User | undefined | null;
@@ -21,6 +23,8 @@ export const AnalyticsPageContent = ({
   me,
   sourceIncomes: initialSourceIncomes,
 }: AnalyticsPageContentProps) => {
+  const isDesktop = useIsBreakpoint(Breakpoints.SM);
+
   const { data: transactionsBySourceName } =
     api.transactions.getBySourceName.useQuery(
       {},
@@ -41,10 +45,26 @@ export const AnalyticsPageContent = ({
 
   return (
     <div>
-      <div className="mb-4">
-        <h1 className="text-lg font-bold leading-6 text-gray-900">
-          Income by source
-        </h1>
+      <div className="mb-12 flex flex-col gap-6 sm:flex-row">
+        <div className="justify-space-between flex items-center gap-12 rounded-2xl bg-white px-6 py-4">
+          <div>
+            <p className="text-3xl font-semibold leading-tight text-primary-blue sm:text-5xl">
+              Be Smart
+            </p>
+            <p className="text-3xl font-semibold leading-tight text-primary-green sm:text-5xl">
+              Use
+            </p>
+            <p className="text-3xl font-semibold leading-tight text-primary-blue sm:text-5xl">
+              Graph
+            </p>
+          </div>
+
+          {isDesktop && (
+            <div>
+              <PieChartHeroIllustrationDesktop />
+            </div>
+          )}
+        </div>
       </div>
 
       <IncomeBySourcePieChart
